a1fdca4f65cb9d2cbfc711288183c5a99faf3d2f
muhammadolimi <neidmine@gmail.com>
2026-05-01 21:36:00 +0500

    add: Movie Model

---
+++ src/KinoDom.API/Data/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace KinoDom.API.Data.Models
+{
+    public class Movie : BaseEntity
+    {
+        public String Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public int? DurationMinutes { get; set; }
+        public String? PosterUrl { get; set; }
+        public String? TrailerUrl { get; set; }
+        public float? AverageRating { get; set; }
+
+    }
+}
