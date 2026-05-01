1f44157d6be4e1898a2b8b4da8ed4e8efc72024d
Shukrulloh <152027751+IskandarovShukrulloh@users.noreply.github.com>
2026-05-01 22:14:49 +0500

    add: Genre & MovieGenre, M-2-M relation

---
+++ src/KinoDom.API/Data/DTOs/Genre/GenreDto.cs
@@ -0,0 +1,8 @@
+﻿namespace KinoDom.API.Data.DTOs.Genre
+{
+    public class GenreDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/KinoDom.API/Data/Models/Genre.cs
@@ -0,0 +1,10 @@
+﻿namespace KinoDom.API.Data.Models
+{
+    public class Genre : BaseEntity
+    {
+        public string Name { get; set; }
+
+        // Navigation property for the many-to-many relationship with Movie
+        public ICollection<MovieGenre> MovieGenres { get; set; }
+    }
+}
