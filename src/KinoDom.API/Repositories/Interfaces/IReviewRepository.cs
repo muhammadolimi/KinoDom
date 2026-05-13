a287319d32bc3d17641d5d0b6f6d1429de5179ba
muhammadolimi <neidmine@gmail.com>
2026-05-13 16:04:17 +0500

    feat: added ReviewRepository

---
+++ src/KinoDom.API/Repositories/Interfaces/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using KinoDom.API.Data.Models;
+
+namespace KinoDom.API.Repositories.Interfaces
+{
+    public interface IReviewRepository
+    {
+        Task<List<Review>> GetAllAsync();
+        Task<Review?> GetByIdAsync(Guid id);
+        Task<Review> CreateAsync(Review review);
+        Task<Review?> UpdateAsync(Guid id, Review review);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
