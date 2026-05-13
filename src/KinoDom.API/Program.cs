818b13811ad00092bf7d39c15eff6f6701f4d40c
Alexey Barnyshev <alexeibarnyshev@yandex.ru>
2026-05-13 20:44:06 +0500

    Add #63 - rating repository implementation

--- src/KinoDom.API/Program.cs
+++ src/KinoDom.API/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 builder.Services.AddScoped<IGenreRepository, GenreRepository>();
+builder.Services.AddScoped<IRatingRepository, RatingRepository>();
 
 // Services
 // to be added later
