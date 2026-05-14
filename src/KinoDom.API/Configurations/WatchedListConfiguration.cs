7e808d8bfc7b38236764eb8c6976e2607938c6a1
muhammadolimi <neidmine@gmail.com>
2026-05-14 15:20:19 +0500

    feat: added Entity Configuration for WatchedList model. fix: property name to plural in User model

---
+++ src/KinoDom.API/Configurations/WatchedListConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using KinoDom.API.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KinoDom.API.Configurations
+{
+    internal class WatchedListConfiguration : IEntityTypeConfiguration<WatchedList>
+    {
+        public void Configure(EntityTypeBuilder<WatchedList> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.HasOne(x => x.Movie)
+                .WithMany(m => m.WatchedLists)
+                .HasForeignKey(x => x.MovieId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.User)
+                .WithMany(u => u.WatchedLists)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
