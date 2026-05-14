283ae4dc5e5e4dffa48046b3935e869e3c484b31
Shukrulloh <152027751+IskandarovShukrulloh@users.noreply.github.com>
2026-05-14 23:31:06 +0500

    feat: MovieGenre DTO & Mapper

---
+++ src/KinoDom.API/Data/DTOs/MovieGenre/MovieGenreDto.cs
@@ -0,0 +1,8 @@
+﻿namespace KinoDom.API.Data.DTOs.MovieGenre
+{
+    public class MovieGenreDto
+    {
+        public Guid MovieId { get; set; }
+        public Guid GenreId { get; set; }
+    }
+}
---
+++ src/KinoDom.API/Mappers/MovieGenreMapper.cs
@@ -0,0 +1,25 @@
+﻿using KinoDom.API.Data.DTOs.MovieGenre;
+using KinoDom.API.Data.Models;
+
+namespace KinoDom.API.Mappers
+{
+    public static class MovieGenreMapper
+    {
+        public static MovieGenreDto ToDto(this MovieGenre movieGenre)
+        {
+            return new MovieGenreDto
+            {
+                MovieId = movieGenre.MovieId,
+                GenreId = movieGenre.GenreId
+            };
+        }
+        public static MovieGenre ToEntity(this CreateMovieGenreDto dto)
+        {
+            return new MovieGenre
+            {
+                MovieId = dto.MovieId,
+                GenreId = dto.GenreId
+            };
+        }
+    }
+}
