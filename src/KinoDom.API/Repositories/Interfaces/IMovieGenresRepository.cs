012e77a967f440651544571a10562de5fdbbedcb
Shukrulloh <152027751+IskandarovShukrulloh@users.noreply.github.com>
2026-05-22 20:27:43 +0500

    refactor: remove GetByMovieAndGenreAsync method for simplicity

--- src/KinoDom.API/Repositories/Interfaces/IMovieGenresRepository.cs
+++ src/KinoDom.API/Repositories/Interfaces/IMovieGenresRepository.cs
@@ -1,25 +1,13 @@
-﻿using KinoDom.API.Data.Models;
+using KinoDom.API.Data.Models;
 
 namespace KinoDom.API.Repositories.Interfaces
 {
     public interface IMovieGenreRepository
     {
-        // Get all movie-genre relations 
         Task<List<MovieGenre>> GetAllAsync();
-
-        // Get specific movie-genre pair by both IDs
-        Task<MovieGenre?> GetByMovieAndGenreAsync(Guid movieId, Guid genreId);
-
-        // Genres for a specific movie
         Task<List<MovieGenre>> GetByMovieIdAsync(Guid movieId);
-
-        // Movies for a specific genre
         Task<List<MovieGenre>> GetByGenreIdAsync(Guid genreId);
-
-        // New movie-genre relation
         Task<MovieGenre> CreateAsync(MovieGenre movieGenre);
-
-        // Remove specific relations by both id
         Task<bool> DeleteAsync(Guid movieId, Guid genreId);
     }
 }
