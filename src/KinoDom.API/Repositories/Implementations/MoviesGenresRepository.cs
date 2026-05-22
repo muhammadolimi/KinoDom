f125f720d3d0f21dbf35d54006336208087df536
Shukrulloh <152027751+IskandarovShukrulloh@users.noreply.github.com>
2026-05-22 20:27:56 +0500

    refactor: remove GetByMovieAndGenreAsync method for simplicity

--- src/KinoDom.API/Repositories/Implementations/MoviesGenresRepository.cs
+++ src/KinoDom.API/Repositories/Implementations/MoviesGenresRepository.cs
@@ -1,4 +1,4 @@
-﻿using KinoDom.API.Data;
+using KinoDom.API.Data;
 using KinoDom.API.Data.Models;
 using KinoDom.API.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +13,7 @@ namespace KinoDom.API.Repositories.Implementations
         {
             _context = context;
         }
+
         public async Task<List<MovieGenre>> GetAllAsync()
         {
             return await _context.MovieGenres
@@ -21,16 +22,6 @@ namespace KinoDom.API.Repositories.Implementations
                 .ToListAsync();
         }
 
-        // Find specific relationship by movieId and genreId (composite key)
-        public async Task<MovieGenre?> GetByMovieAndGenreAsync(Guid movieId, Guid genreId)
-        {
-            return await _context.MovieGenres
-                .Include(mg => mg.Movie)
-                .Include(mg => mg.Genre)
-                .FirstOrDefaultAsync(mg => mg.MovieId == movieId && mg.GenreId == genreId);
-        }
-
-        // genres assigned to one movie
         public async Task<List<MovieGenre>> GetByMovieIdAsync(Guid movieId)
         {
             return await _context.MovieGenres
@@ -39,7 +30,6 @@ namespace KinoDom.API.Repositories.Implementations
                 .ToListAsync();
         }
 
-        // movies by genre
         public async Task<List<MovieGenre>> GetByGenreIdAsync(Guid genreId)
         {
             return await _context.MovieGenres
@@ -48,29 +38,23 @@ namespace KinoDom.API.Repositories.Implementations
                 .ToListAsync();
         }
 
-        // Create new movie-genre relationship
         public async Task<MovieGenre> CreateAsync(MovieGenre movieGenre)
         {
             await _context.MovieGenres.AddAsync(movieGenre);
             await _context.SaveChangesAsync();
-           
             return movieGenre;
         }
+
         public async Task<bool> DeleteAsync(Guid movieId, Guid genreId)
         {
-            // Find the relationship to delete
             var movieGenre = await _context.MovieGenres
                 .FirstOrDefaultAsync(mg => mg.MovieId == movieId && mg.GenreId == genreId);
 
-            // If not found, return false
             if (movieGenre == null)
                 return false;
 
-            // Remove and save
             _context.MovieGenres.Remove(movieGenre);
-
             await _context.SaveChangesAsync();
-
             return true;
         }
     }
